03290d596cf5066c7eff7d41f91ffe2159192e73
kamiya-daiki <kamiya.d02@gmail.com>
2026-03-05 11:52:49 +0900

    - 履歴テーブルの構成やPKなどを修正

---
+++ 03_詳細設計/テーブル定義書/03_init_users.sql
@@ -0,0 +1,32 @@
+------------------------------
+-- ユーザーテーブル
+------------------------------
+CREATE TABLE users (
+    -- ID
+    id                      BIGSERIAL,
+    -- ユーザーID
+    user_id                 VARCHAR(50) NOT NULL,
+    -- メールアドレス
+    email                   VARCHAR(50) NOT NULL,
+    -- パスワード(BCryptでハッシュ化済)
+    user_password           VARCHAR(100) NOT NULL,
+    -- 有効・無効
+    enabled                 boolean NOT NULL DEFAULT true,
+    -- ユーザー作成日時
+    create_datetime         timestamp DEFAULT CURRENT_TIMESTAMP,
+    -- 最終パスワード更新日時
+    last_update_password_datetime   timestamp DEFAULT CURRENT_TIMESTAMP,
+    -- 最終ログイン日時
+    last_login_datetime     timestamp DEFAULT CURRENT_TIMESTAMP,
+    -- 権限
+    authority               integer NOT NULL DEFAULT 0,
+    -- 画面モード
+    screen_mode             integer NOT NULL DEFAULT 0,
+
+    -- 外部キー設定
+    FOREIGN KEY (authority) REFERENCES authority_master(authority_id),
+    FOREIGN KEY (screen_mode) REFERENCES screen_mode_master(screen_mode_id),
+
+    -- 主キー設定
+    PRIMARY KEY (id)
+);
